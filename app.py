8850637eff64aedaa54533063cf20aa6c3de6f07
GuilhermeHobbs <guilhermehobbs@hotmail.com>
2025-02-13 21:57:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # This enables CORS for all routes
 
-@app.route('/ask-name')
+@app.route('/move')
 def ask_name():
-    question = request.args.get('question', '')
-    if question == "The Name?":
-        return "It's meee"
-    else:
-        return "Invalid question"
+    a = request.args.get('a', '')
+    b = request.args.get('b', '')
+    
+    return a+b
+   
