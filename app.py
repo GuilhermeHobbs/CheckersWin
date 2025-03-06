dadf147265098de76c316933b702357068b60bbe
GuilhermeHobbs <guilhermehobbs@hotmail.com>
2025-03-06 13:09:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 from flask import Flask, request
 from flask_cors import CORS
 
+#import numpy as np
+#import torch
+#import torch.nn as nn
+#from torch.nn import functional as F
+
 app = Flask(__name__)
 CORS(app)  # This enables CORS for all routes
 
-print("Uéeee")
-
+  
 @app.route('/move')
 def ask_name():
-    print("aloooo")
-    #logits, _ = m(context.int())
-    #print(logits)
-
-    print("Eh assim")
+    print("Eiiiii")
     
-    return "hello"
+    return "HELLO"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)  # Render requires explicit host/port
